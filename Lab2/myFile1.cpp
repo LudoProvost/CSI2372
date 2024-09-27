7cc3b3df316904d7174fb7631e6fe31ba9594ba2
LudoProvost <ludovic.provost8@gmail.com>
2024-09-27 15:25:19 -0400

    solved exercise 1

---
+++ Lab2/myFile1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int* arr, int size) {
+    cout << "[";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << ", ";
+    }
+    cout << "\b\b]\n";
+}
+
+int main(void) {
+    int tab[10];
+    int* p;
+    for (int i = 0; i < 10; i++) {
+        tab[i] = i * i;
+    }
+
+    cout << tab[2] << endl; // print value 4 before it is replaced by tab[1]
+
+    tab[2] = tab[1]; // tab[2] is set to 1
+    
+    cout << tab[2] << endl; // print value 1 (1/5)
+    
+    tab[2] = *(tab + 1); // tab[2] is set to 1 (again)
+    
+    cout << tab[2] << endl; // print value 1 (2/5)
+
+    *(tab + 2) = tab[1]; // tab[2] is set to 1 (again)
+
+    cout << tab[2] << endl; // print value 1 (3/5)
+
+    *(tab + 2) = *(tab + 1); // tab[2] is set to 1 (again)
+
+    cout << tab[2] << endl; // print value 1 (4/5)
+
+    p = &tab[0]; // p points to tab[0]
+    p = tab + 1; // p points to tab[1]
+    tab[4] = *p; // tab[4] is set to 1 (the value at p)
+    cout << tab[4] << endl; // print value 1 (5/5)
+}
