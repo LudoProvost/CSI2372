0da1a686b6a26b0b61787dd080ab812a7dfa9220
ChrisKefeli <64187648+ChrisKefeli@users.noreply.github.com>
2024-10-05 22:51:35 -0400

    Finished exercise 3

--- Lab2/myFile3.cpp
+++ Lab2/myFile3.cpp
@@ -3,17 +3,30 @@
 #include <iostream>
 using namespace std;
 
-int** triangleInf(int n)
-{
-	//YOUR CODE
+int** triangleInf(int n){
+
+	int** tab = new int*[n]; // Create array of "pointers to ints" of size n
+    
+    for(int i = 0; i < n; ++i){ // Create new array for each element in tab to point to
+        tab[i] = new int[i + 1];
+        
+        for(int j = 0; j <= i; ++j){
+            if(j == 0 || j == i){ 
+                tab[i][j] = 1; // The first and last elements of each row are set to 1
+            }else{ 
+                tab[i][j] = tab[i-1][j-1] + tab[i-1][j]; // Everyother element is the sum of the two elements above it 
+            }
+        }
+    }
+    return tab;
 }
 
 int main() {
 	int** tab;
 	const int size = 10;
 	tab = triangleInf(size);
-	for (int i = 0; i < size; i++)
-	{
+
+	for (int i = 0; i < size; i++){
 		for (int j = 0; j <= i; j++)
 			cout << tab[i][j] << " ";
 		cout << endl;
