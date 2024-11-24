26b12e1e3221b0cffb10483849034d5eb95d7889
LudoProvost <ludovic.provost8@gmail.com>
2024-11-24 15:35:18 -0500

    implemented Chain

--- Project/Chain.cpp
+++ Project/Chain.cpp
@@ -1,2 +1,72 @@
 #include "headers/Chain.h"
 using namespace std;
+
+int Chain_Base::getNumCard() const {
+    return chain.size();
+}
+
+string Chain_Base::getChainType() const {
+    return type;
+}
+
+template <typename T>
+Chain<T>::Chain() {
+    T card; // create card item of type T to get string name of T
+    type = card.getName(); // set type to name of card
+}
+
+//TODO: this function
+template <typename T>
+Chain<T>::Chain(istream& in, const CardFactory* cf) {
+
+}
+
+template <typename T>
+int Chain<T>::sell() {
+    T cardType;
+    int numCard = getNumCard();
+
+    // find value of chain
+    for (int coinNum = 4; coinNum >= 1; coinNum--) {
+        int cardsPerCoin = cardType.getCardsPerCoin(i);
+
+        // edge case for invalid value of chains, just ignore the iteration
+        if (cardsPerCoin == -1) {
+            continue;
+        }
+        
+        if (numCard >= cardsPerCoin) {
+            return coinNum;
+        }
+    }
+
+    // if coinNum wasn't returned in the for loop, the player doesn't have enough cards to get coins.
+    return 0;
+}
+
+template <typename T>
+Chain<T>& Chain<T>::operator+=(Card* c) {
+    
+    //TODO: may need an edge case to set type of chain to type of c if chain is empty
+
+    // throw type exception if type of c does not match T
+    //TODO: idk if this needs a return
+    if (!(c->getName()).compare(getChainType())) {
+        throw runtime_error("IllegalType");
+    }
+
+    chain.push_back(c);
+    return *this;
+}
+
+ostream& operator<<(ostream& out, const Chain<Card*>& chain) {
+    // print full name and tab
+    out << chain.getChainType() << "\t";
+
+    // print number of cards
+    int numCard = chain.getNumCard();
+    for (int i = 0; i < numCard; i++) {
+        out << chain.getChainType()[0] << " ";
+    }
+    return out;
+}
--- Project/headers/Chain.h
+++ Project/headers/Chain.h
@@ -1,8 +1,32 @@
 #ifndef CHAIN_H
 #define CHAIN_H
+#include <iostream>
+#include <vector>
+using namespace std;
 
-class Chain{
+#include "Card.h"
+#include "CardFactory.h"
 
+class Chain_Base {
+    protected:
+        vector<Card*> chain;
+        string type;
+    public:
+        virtual int getNumCard() const;
+        virtual string getChainType() const;
+};
+
+template <typename T>
+class Chain : public virtual Chain_Base{
+    public:
+        Chain();
+        Chain(istream& in, const CardFactory* cf);
+
+        int sell();
+
+        Chain<T>& operator+=(Card* c);
+
+        friend ostream& operator<<(ostream& out, const Chain<Card*>& chain);
 };
 
 #endif 
